9673c1c06b84c810867a46c6f99b176db9ebe085
haein-side <kimhi1640@gmail.com>
2022-05-18 20:04:16 +0000

    proxy 완료

---
+++ .proxy/tiny.c
@@ -0,0 +1,362 @@
+/* $begin tinymain */
+/*
+ * tiny.c - A simple, iterative HTTP/1.0 Web server that uses the
+ *     GET method to serve static and dynamic content.
+ * HTTP 프로토콜: 클라이언트-서버 간 데이터를 주고 받는 응용 계층의 프로토콜
+ * Updated 11/2019 droh
+ *   - Fixed sprintf() aliasing issue in serve_static(), and clienterror().
+ */
+#include "csapp.h"
+
+/*
+* main: 명령줄에서 넘겨받는 포트로의 연결 요청을 받음
+* doit: 한 개의 HTTP 트랜잭션을 처리 (원하는 요청과 응답을 모두 doit에 넣음)
+* read_requesthdrs: 요청 헤더를 읽고 무시
+* parse_uri: HTTP URI를 분석
+* serve_static: 정적 컨텐츠를 클라이언트에게 서비스
+* serve_dynamic: 동적 컨텐츠를 클라이언트에게 서비스
+* clienterror: 에러 메시지를 클라이언트에게 보냄
+*/
+
+void doit(int fd);
+void read_requesthdrs(rio_t *rp);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int fd, char *filename, int filesize, char *method);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs, char *method);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
+                 char *longmsg);
+
+/* port 번호를 인자로 받아 클라이언트의 요청이 올 때마다 새로 연결 소켓을 만들어 doit() 함수를 호출한다. */
+int main(int argc, char **argv) {
+  /*
+  * argc: 메인 함수에 전달 되는 데이터의 수
+  * argv: 메인 함수에 전달 되는 실질적인 정보
+  */
+  int listenfd, connfd; 
+  // 듣기식별자: 클라이언트로부터의 연결 요청 승낙 가능 상태 시 생성됨
+  // 연결식별자: 서버가 연결 요청을 수락할 때마다 생성되고, 서버가 클라이언트에 서비스 하는 동안에만 존재
+  char hostname[MAXLINE], port[MAXLINE];
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr; // 클라이언트에서 연결 요청을 보내면 알 수 있는 클라이언트 연결 소켓 주소
+
+  // 인자 2개 받아야 함
+  if (argc != 2) { 
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1); // 에러메시지 종료
+  }
+
+  /* 해당 포트 번호에 해당하는 듣기 소켓 식별자를 열어줌 */
+  listenfd = Open_listenfd(argv[1]); 
+
+  /* 클라이언트의 요청이 올 때마다 새로 연결 소켓을 만들어 doit() 호출*/
+  while (1) {
+    /* 클라이언트에게서 받은 연결 요청을 accept한다. connfd = 서버 연결 식별자 */
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);  // line:netp:tiny:accept
+
+    /* 연결이 성공했다는 메세지를 위해 Getnameinfo를 호출하면서 hostname과 port가 채워짐 */
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); 
+    // 소켓 주소 구조체를 대응되는 호스트와 서비스 이름 스트링으로 변환
+    // 내 컴퓨터 하나에서 요청해서 ip 주소는 같지만 포트 번호가 제각기 다름
+    printf("Accepted connection from (%s, %s)\n", hostname, port);
+
+    /* doit 함수를 실행 */
+    doit(connfd);   // line:netp:tiny:doit
+
+    /* 서버 연결 식별자를 닫아줌 */
+    Close(connfd);  // line:netp:tiny:close
+
+  /*
+  * addrinfo의 구조
+  * int flags             - 추가적인 옵션을 정의 할 때 사용함. 여러 flag를 OR연산해서 생성
+  * int family            - address family를 나타냄. AF_INET(IPv4), AF_INET6(IPv6), AF_UNSPEC(정해지지 않았을 때)
+  * int socktype          - socktype을 나타냄. SOCK_STREAM(스트림 소켓, 즉, TCP), SOCK_DGRAM(데이터그램 소켓, 즉, UDP)
+  * int protocol          - IPv4와 IPv6에 대한 IPPROTO_xxx와 같은 값을 가짐. 0을 넣을 시 모든 프로토콜 관련 정보를 얻을 수 있고, 특정 프로토콜을 지정해 줄 수 있음
+  * socklen_t addrlen     - Length of socket address. socket 주소인 addr의 길이를 가짐
+  * sokaddr *addr         - Socket address for socket. sockaddr 구조체 포인터
+  * char *canonname       - 호스트의 canonical name을 나타냄, hostname을 가리키는 포인터
+  * addrinfo *next        - 주소정보 구조체 adrinfo는 linked list이다. 즉, 다음 노드를 가리키는 포인터
+  */
+
+  }
+}
+
+/* 클라이언트의 HTTP 요청 라인을 확인해 정적, 동적 컨텐츠인지를 구분하고 각각의 서버에 보낸다. */
+void doit(int fd) {
+  int is_static;      // 정적 vs. 동적 컨텐츠
+  struct stat sbuf;   // 파일의 정보를 담아줄 변수
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE]; // 클라이언트에게서 받은 요청(rio)으로 채워진다.
+  char filename[MAXLINE], cgiargs[MAXLINE]; 
+  rio_t rio;
+
+  /* Read request line and headers */
+  /* 클라이언트가 rio로 보낸 request 라인과 헤더를 읽고 분석한다. */
+  Rio_readinitb(&rio, fd); // fd의 내용을 rio 구조체에 초기화. rio 안의 내부 버퍼와 fd(서버의 connfd) 연결
+  Rio_readlineb(&rio, buf, MAXLINE); //  rio(==connfd)에 있는 string 한 줄(응답 라인)을 읽고 buf로 옮김
+
+  /* 사용자 버퍼인 buf에 connfd의 값들이 들어간 것 */
+
+  printf("Request headers:\n");
+  printf("%s", buf); // 요청 라인 buf = "GET /godzilla.gif HTTP/1.1\0"을 표준 출력만 해줌!
+  sscanf(buf, "%s %s %s", method, uri, version); // buf에서 포멧을 지정하여 읽어옴
+
+  /* 요청 method가 GET이나 HTTP가 아니면 종료. main으로 가서 연결 닫고 다음 요청 기다림 */
+  if (!(strcasecmp(method, "GET") || strcasecmp(method, "HTTP"))) { // 대소문자를 구분하지 않고 스트링 비교
+    clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
+  }
+
+  /* 요청 라인을 뺀 나머지 요청 헤더들을 프린트 */
+  // 위에서 요청 라인을 한 줄 읽어서 포인터가 그 다음 요청 헤더들을 가리킴
+  // 포인터가 컨텐츠를 가리키도록 요청 헤더들을 모두 읽어줌
+  read_requesthdrs(&rio);
+
+  /* 컨텐츠의 유형(정적, 동적)을 파악한 후 각각의 서버에 보냄 */
+  /* Parse URI from GET request */
+  is_static = parse_uri(uri, filename, cgiargs);  // 클라이언트 요청 라인에서 받아온 uri를 이용해 정적/동적 컨텐츠를 구분 (정적이면 1)
+  
+  /* stat(file, *buffer) : file의 상태를 buffer에 넘긴다. */
+  /* 여기서 filename : 클라이언트가 요청한 서버의 컨텐츠 디렉토리 및 파일 이름 */
+  if (stat(filename, &sbuf) < 0) { // 요청받은 filename을 찾아서 그 파일의 속성을 sbuf에 넘김 
+    clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+    return;
+  }
+
+  if (is_static) { /* Serve static content */
+    if (! (S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { // !(정규 파일이다) || !(읽기 권한 있다) (둘 중 하나라도 만족 못하면)
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+      return;
+    }
+    // 읽기 가능한 정적 파일의 filename 확인해보기
+    // printf("정적 filename 입니다 %s \n", filename);
+
+    // 정적 서버에 연결식별자, 파일명, 해당 파일 사이즈 보냄
+    serve_static(fd, filename, sbuf.st_size, method);
+  }
+  else { /* Serve dynamic content */
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)){ // !(정규 파일이다) || !(실행 권한 있다) (둘 중 하나라도 만족 못하면)
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+      return;
+    }
+    // 실행 가능한 정적 파일의 filename 확인해보기
+    // printf("동적 filename 입니다 %s \n", filename);
+
+    // 동적 서버에 연결식별자, 파일명, 해당 파일 사이즈 보냄
+    serve_dynamic(fd, filename, cgiargs, method); 
+  }
+}
+
+/* HTTP 응답을 응답라인과 응답 본체(HTML)를 서버 소켓을 통해 클라이언트에 보낸다. */
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
+  
+  char buf[MAXLINE], body[MAXBUF];
+  
+  /* 응답 본체: 요청한 컨텐츠들이 포함되고 화면에서 볼 수 있음 */
+  /* Build the HTTP response body */
+  sprintf(body, "<html><title>Tiny Error</title>"); // 출력할 결과값을 변수(body)에 저장하는 함수
+  sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+  /* 응답 헤더: 클라이언트는 개발자 도구로 들어가면 볼 수 있음 */
+  /* Print the HTTP response */
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf)); // buf에 있는 값이 fd로 써짐 (파일이 수정됨)
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf)); 
+
+  /* HTML 컨텐츠로 담은 body가 fd로 써짐 -> HTML 수정됨 */
+  Rio_writen(fd, body, strlen(body));
+
+}
+
+/* 클라이언트가 버퍼 rp에 보낸 나머지 요청 헤더들을 그냥 프린트한다 */
+void read_requesthdrs(rio_t *rp) {
+  char buf[MAXLINE];
+
+  Rio_readlineb(rp, buf, MAXLINE);
+  while(strcmp(buf, "\r\n")) { // 더 읽을 거 있을 수 있으니까 \r\n 을 기준으로 끊기
+    Rio_readlineb(rp, buf, MAXLINE); // rp(==connfd)에 있는 string 한 줄씩 모두 buf로 옮김
+    printf("%s", buf);
+  }
+  return;
+}
+
+/* uri를 받아 요청받은 파일의 이름(filename)과 요청 인자(cgiarg)를 채워준다. */
+int parse_uri(char *uri, char *filename, char *cgiargs) {
+  char *ptr;
+
+  /* uri에 cgi-bin이 없다면, 즉 정적 컨텐츠를 요청한다면 1을 리턴한다.*/
+  if (!strstr(uri, "cgi-bin")) { /* Static Content */
+    // strstr(str1, str2);
+    // str1에서 str2와 일치하는 문자열이 있는지 확인 -> 일치하는 문자열이 있으면 해당 위치의 포인터(char*) 반환
+    strcpy(cgiargs, ""); // 정적이니까 cgiargs는 아무것도 없다.
+    strcpy(filename, "."); // 현재경로에서부터 시작 ./path ~~
+    strcat(filename, uri); // filename에 uri를 연결
+
+    /* 예시
+      uri : /godzilla.jpg
+      ->
+      cgiargs : 
+      filename : ./godzilla.jpg
+    */
+
+    // 만약 uri뒤에 '/'이 있다면 그 뒤에 home.html을 붙인다.
+    // 내가 브라우저에 http://localhost:8000만 입력하면 바로 뒤에 '/'이 생기는데,
+    // '/' 뒤에 home.html을 붙여 해당 위치 해당 이름의 정적 컨텐츠가 출력
+    if (uri[strlen(uri)-1] == '/') {
+      strcat(filename, "home.html"); // home.html을 filename에 연결
+    }
+
+    // 정적 컨텐츠면 1 리턴
+    return 1;
+  }
+  else { /* Dynamic Content */
+    ptr = index(uri, '?'); // uri 중 ?가 있는 인덱스를 ptr에 넣어줌
+
+    // '?'가 있으면 cgiargs를 '?' 뒤 인자들과 값으로 채워주고 ?를 NULL로 만든다.
+    if (ptr) {
+      strcpy(cgiargs, ptr+1); // 뒤에 있는 문자열 전체를 앞에 있는 변수로 복사하는 함수
+      *ptr = '\0';
+    }
+    else { // '?'가 없으면 그냥 아무것도 안 넣어준다.
+      strcpy(cgiargs, "");
+    }
+    strcpy(filename, "."); // 현재 디렉토리에서 시작
+    strcat(filename, uri); // uri 넣어준다.
+
+    /* 예시
+      uri : /cgi-bin/adder?123&123
+      ->
+      cgiargs : 123&123
+      filename : ./cgi-bin/adder
+    */
+
+    return 0;
+  }
+}
+
+/*
+* 클라이언트가 원하는 정적 컨텐츠 디렉토리를 받아온다. 
+* 응답 라인과 헤더를 작성하고 서버에게 보낸다. 
+* 그 후 정적 컨텐츠 파일을 읽어 그 응답 본체를 클라이언트에 보낸다.
+*/
+void serve_static(int fd, char *filename, int filesize, char *method) {
+  int srcfd;
+  char *srcp, filetype[MAXLINE], buf[MAXBUF];
+  rio_t file_rio;
+  int n;
+  char *file;
+
+  /* Send response headers to client 클라이언트에게 응답 헤더 보내기*/
+  /* 응답 라인과 헤더 작성 */
+  get_filetype(filename, filetype); // 파일 타입 찾아오는 함수 호출
+  sprintf(buf, "HTTP/1.0 200 OK\r\n"); // 출력할 결과값을 변수(buf)에 저장하는 함수 // 응답 라인 작성
+  sprintf(buf, "%sServer: Tiny Web Server\r\n", buf); // 응답 헤더 작성
+  sprintf(buf, "%sConnection: close\r\n", buf);
+  sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+  sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+
+  /* 응답 라인과 헤더를 클라이언트에게 보냄 */
+  Rio_writen(fd, buf, strlen(buf)); // buf에 있는 응답 라인과 헤더가 fd로 써짐 (파일이 수정되어 클라이언트가 받음)
+  printf("Response headers:\n");
+  printf("%s", buf); // 커멘드 창에 buf의 내용 프린트
+
+  // /* 응답 바디를 클라이언트에게 보냄 */
+  // srcfd = Open(filename, O_RDONLY, 0); // filename의 이름을 갖는 파일을 읽기 권한으로 불러온다.
+  // srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0); // 메모리에 파일 내용을 동적할당한다.
+  // Close(srcfd); // 열어줬던 파일을 닫음
+  // Rio_writen(fd, srcp, filesize); // 메모리 동적할당된 파일 내용을 fd에 써줌 (클라이언트가 응답 바디 볼 수 있음)
+  // Munmap(srcp, filesize); // mmap 함수로 매핑해준 메모리 영역을 해제해줌
+  
+  /* 만약 메서드가 HEAD라면, 응답 바디를 만들지 않고 끝낸다.*/
+  if (strcasecmp(method, "HEAD") == 0){
+    return;
+  }
+
+  /* 메서드가 GET이면 응답 바디를 만들고 클라이언트에게 보내줌 */
+  // 읽을 수 있는 파일로 열기
+  srcfd = Open(filename, O_RDONLY, 0);
+  // 파일을 어떤 메모리 공간에 대응시키고 첫주소를 리턴
+  // srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  file = (char *)malloc(filesize); // filesize만큼 공간 할당
+  Rio_readinitb(&file_rio, srcfd); // srcfd 파일을 file_rio에 연결해줌
+  Rio_readnb(&file_rio, file, filesize); // file_rio에 있는 걸 file에 한 줄씩 복사해줌
+  Rio_writen(fd, file, filesize); // file(메모리)에 있는 값이 fd로 써져서 클라이언트에 보냄 (클라이언트가 응답 바디 볼 수 있음)
+  Close(srcfd);
+  free(file);
+  //대응시킨 녀석을 풀어준다. 유효하지 않은 메모리로 만듦
+  // void *mmap(void *start, size_t length, int prot, int flags, int fd, off_t offset);
+  // int munmap(void *start, size_t length);
+  // Munmap(srcp, filesize);
+
+}
+
+/* get_filetype - Derive file type from filename */
+// filename을 조사해 웹 서버에서 읽을 수 있는 각각의 식별자에 맞는 MIME 타입을 filetype에 입력해준다.
+void get_filetype(char *filename, char *filetype) {
+    if (strstr(filename, ".html")) // 브라우저 클라이언트한테 요청 받은 것
+        strcpy(filetype, "text/html"); // 웹 서버에서 읽을 수 있는 MIME 타입으로 넣어줌
+    else if (strstr(filename, ".gif"))
+        strcpy(filetype, "image/gif");
+    else if (strstr(filename, ".png"))
+        strcpy(filetype, "image/png");
+    else if (strstr(filename, ".jpg"))
+        strcpy(filetype, "image/jpeg");
+    else if (strstr(filename, ".mp4"))
+        strcpy(filetype, "video/mp4");
+    else if (strstr(filename, ".mpeg"))
+        strcpy(filetype, "video/mpeg");
+    else
+        strcpy(filetype, "text/plain");
+}
+
+
+/*
+* 클라이언트가 원하는 동적 컨텐츠 디렉토리를 받아온다. 
+* 응답 라인과 헤더를 작성하고 서버에게 보낸다. 
+* CGI 자식 프로세스를 fork하고 그 프로세스의 표준 출력을 클라이언트 출력과 연결한다.
+*/
+void serve_dynamic(int fd, char *filename, char *cgiargs, char *method) {
+    // fd: 연결 식별자, filename: uri의 파일경로 (별칭), cgiargs: 쿼리스트링
+    char buf[MAXLINE], *emptylist[] = {NULL};
+
+    // 서버가 GET /cgi-bin/adder?15000&213 HTTP/1.1 과 같은 요청을 받음
+
+    /* Return first part of HTTP response */
+    /* Send response headers to client 클라이언트에게 응답 헤더 보내기*/
+    /* 응답 라인과 헤더 작성 */
+    sprintf(buf, "HTTP/1.0 200 OK\r\n"); // 버퍼에 넣음
+    Rio_writen(fd, buf, strlen(buf)); // 버퍼에서 꺼내서 클라이언트 fd에 수정해줌
+    sprintf(buf, "Server: Tiny Web Server\r\n"); 
+    Rio_writen(fd, buf, strlen(buf)); 
+
+    // 자식 프로세스 생성, 실행 자식만 조건문 실행 (반환값이 0이면 자식 프로세스)
+    // Fork() 실행 시 실행하면 부모 프로세스와 자식 프로세스가 동시에 실행됨
+    if (Fork() == 0) 
+    {
+        // 자식 프로세스의 CGI 환경변수 QUERY_STRING을 "15000&213"으로 설정해줌
+        // 1이면 원래 있던거 지우고 다시 넣기
+        // adder 프로그램은 런 타임에 리눅스 getenv()로 이 값을 참조 가능
+        setenv("QUERY_STRING", cgiargs, 1); // 10&20
+
+        /* 요청 메서드를 환경변수에 추가한다. */
+        setenv("REQUEST_METHOD", method, 1);
+
+        // fd에 stdout 넣기 (표준 출력이 fd에 저장되게 만드는 것)
+        // adder에서 fflush로 stdout만 출력하고 끝날 게 아니라, 
+        // fd에 있는 데이터를 수정해줘야 하므로 stdout을 fd에 저장되게 만듦
+        Dup2(fd, STDOUT_FILENO); /* Redirect stdout to client */
+        // 자식은 자식의 표준 출력을 연결 파일 식별자로 재지정
+
+        // filename을 찾아서 /cgi-bin/adder 프로그램을 자식의 컨텍스트에서 실행
+        // environ에 환경변수들 담겨 있음
+        // emptylist는 변수 개수 맞춰주려고 넣어준 임의의 빈 배열
+        Execve(filename, emptylist, environ); /* Run CGI program */
+    }
+    Wait(NULL); // 부모 프로세스가 자식 프로세스 끝날때까지 기다림
+}
--- proxy.c
+++ proxy.c
@@ -2,8 +2,8 @@
 #include "csapp.h"
 
 /* Recommended max cache and object sizes */
-#define MAX_CACHE_SIZE 1049000
-#define MAX_OBJECT_SIZE 102400 // 캐시 하나하나 사이즈
+#define MAX_CACHE_SIZE 1049000 // 캐시 전체 사이즈
+#define MAX_OBJECT_SIZE 102400 // 캐시에 들어갈 객체 하나 사이즈
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
@@ -22,11 +22,23 @@ static const char *host_key = "Host";
 void doit(int fd);
 void parse_uri(char *uri, char *hostname, char *path,int *port);
 void build_http_header(char *http_header, char *hostname, char *path, int port, rio_t *client_rio);
-int connect_endServer(char *hostname, int port, char *http_header);
+int connect_endServer(char *hostname, int port);
 void *thread(void *vargsp);
 void init_cache(void);
 int reader(int connfd, char *url);
 void writer(char *url, char *buf);
+
+/* cache 구조체 */
+typedef struct {
+    char *url;      /* url 담을 변수(같은 요청인지 아닌지 확인) */
+    int *flag;      /* 캐시가 비어있는지(0) 이미 차 있는지(1) 구분할 변수 */
+    int *cnt;       /* 최근 방문 순서 나타내기 위한 변수 - 최근 방문하면 0, 방문 안 할수록 +1 */
+    char *content;  /* 클라이언트에 보낼 내용 담겨있는 변수 */
+} Cache_info;
+
+Cache_info *cache;  /* cache 변수 선언 */
+int readcnt;        /* 현재 크리티컬 섹션에 있는 reader의 수를 세는 변수 */
+sem_t mutex, w; 
 
 /* end server info(브라우저 테스트를 위한) */
 // static const char *end_server_host = "localhost";   /* end server의 hostname은 현재 localhost */
@@ -35,7 +47,7 @@ void writer(char *url, char *buf);
 /*
 * argc: 메인 함수에 전달 되는 데이터의 수
 * argv: 메인 함수에 전달 되는 실질적인 정보 (여기선 proxy에서 연결 대기 상태로 만들어주고 싶은 포트 번호가 들어감)
-* ./proxy 8888
+* ./proxy "8888"이 argv
 */
 int main(int argc, char **argv) {
     /* 프로그램 실행 시 port를 안썼으면 */
@@ -45,24 +57,24 @@ int main(int argc, char **argv) {
         exit(1);
     }
     
-    int listenfd, connfd;                  
+    init_cache();                          /* 캐시 생성 */
+    int listenfd, *connfd;                  
     // 듣기식별자: 클라이언트로부터의 연결 요청 승낙 가능 상태 시 생성됨
     // 연결식별자: 서버가 연결 요청을 수락할 때마다 생성되고, 서버가 클라이언트에 서비스 하는 동안에만 존재
     char hostname[MAXLINE], port[MAXLINE]; /* hostname: 접속한 클라이언트 ip, port: 접속한 클라이언트 port */
     socklen_t clientlen;                   /* socklen_t 는 소켓 관련 매개 변수에 사용되는 것으로 길이 및 크기 값에 대한 정의를 내려준다 */
-    struct sockaddr_storage clientaddr;    
-    // 클라이언트에서 연결 요청을 보내면 알 수 있는 클라이언트 연결 소켓 주소
-    // 어떤 타입의 소켓 구조체가 들어올지 모르기 때문에 충분히 큰 소켓 구조체로 선언 
-    // pthread_t tid;
+    struct sockaddr_storage clientaddr;    /* 클라이언트에서 연결 요청을 보내면 알 수 있는 클라이언트 연결 소켓 주소 */
+                                           /* 어떤 타입의 소켓 구조체가 들어올지 모르기 때문에 충분히 큰 소켓 구조체로 선언 */
+    pthread_t tid;                         /* thread ID는 스레드가 생성되면 스레드 id인 tid가 생성됨 */                          
 
     /* listenfd: 이 포트에 대한 프록시의 듣기 소켓 오픈 */
-    // telnel localhost 8888
+    // telnet localhost 8888
     listenfd = Open_listenfd(argv[1]);
-    pthread_t tid; // 스레드가 생성되면 스레드 id인 tid가 생성됨
 
     while (1) {
         clientlen = sizeof(clientaddr);                                                 /* 소켓 구조체 크기 */
-        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);                       /* 연결 요청 큐에 아무것도 없을 경우 기본적으로 연결이 생길때까지 호출자를 막아둠, 즉 대기 상태 (./proxy 8888 했을 때의 경우) */
+        connfd = Malloc(sizeof(int));
+        *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);                      /* 연결 요청 큐에 아무것도 없을 경우 기본적으로 연결이 생길때까지 호출자를 막아둠, 즉 대기 상태 (./proxy 8888 했을 때의 경우) */
                                                                                         /* telnet localhost 8888로 연결요청이 생기면 연결 요청 큐에 요청이 담김
                                                                                            addr 내에 클라이언트의 소켓주소가 담기고
                                                                                            connfd가 생겨나는 것 */
@@ -71,9 +83,9 @@ int main(int argc, char **argv) {
         // telnet localhost 8888을 실행했을 때 출력됨 (편하게 생각하면 telnet localhost 8888은 클라이언트가 연결요청을 서버에 보낸 것임 -> Accept 함수 실행되고 connfd가 생성됨)
         printf("Accepted connection from (%s, %s)\n", hostname, port);                  /* 어떤 주소와 포트 번호를 가진 client가 들어왔는지 print */
 
-        Ptrhread_create(&tid, NULL, thread, (void *)connfd);
+        Pthread_create(&tid, NULL, thread, (void *)connfd);
 
-        doit(connfd);                                                                   /* 트랜잭션 수행 */
+        // doit(connfd);                                                                /* 트랜잭션 수행 */
         
         /* 연결이 끝났다고 print 하고 식별자(파일 디스크립트)를 닫아줌 */
         // printf("endoffile\n");
@@ -81,13 +93,14 @@ int main(int argc, char **argv) {
     }
 }
 
-void *thread(void *vargs) {
-    int connfd = *((int *)vargs);
-    Pthread_detach(pthread_self()); 
-    // 연결가능한 상태로 스레드가 유지되면 스레드 하나를 종료시켜도 연결된 스레드가 죽을 때까지 기다림
-    // 죽이면 자기 자신만 죽이고 싶어서 자신을 분리해주는 것
-    doit(connfd);
-    Close(connfd);
+void *thread(void *vargs) {                 // 매개변수: 클라이언트와 프록시의 연결식별자
+    int connfd = *((int *)vargs);           // 클라이언트와 프록시가 연결되어 있는 식별자       
+    Pthread_detach(pthread_self());         // 연결가능한 상태로 스레드가 유지되면 스레드 하나를 종료시켜도 연결된 스레드가 죽을 때까지 기다림
+                                            // 죽이면 자기 자신만 죽이고 싶어서 자신을 분리해주는 것
+    free(vargs);                            // 공간 할당 해제
+    doit(connfd);                           // 스레드별로 트랜잭션 수행
+    Close(connfd);                          // 트랜잭션 수행 후 식별자 닫기
+    return NULL;
 }
 
 /* 클라이언트의 요청을 입력받고 요청에 따른 트랜잭션 (알맞은 서버로 요청 보내줌) 처리 */
@@ -99,22 +112,26 @@ void doit(int connfd) {
     char hostname[MAXLINE], path[MAXLINE];                              /* hostname: 클라이언트가 요청한 IP담을 공간, path: 경로 담을 공간 */
     rio_t rio, server_rio;                                              /* rio: client rio 구조체, server_rio: proxy의 rio 구조체 */
     int port;                                                           /* port 담을 변수 */
+    char url[MAXLINE];                                                  /* cache를 read할 때 uri 값을 넣어둔 url 변수로 찾을 수 있음 */
+    char content_buf[MAX_OBJECT_SIZE];                                  /* 서버에서 받아온 내용, 즉 cache에 쓸 내용을 담은 버퍼 */
 
     Rio_readinitb(&rio, connfd);                                        /* rio 구조체 초기화 */
     // telnet localhost 8888해줬을 때 여기서 클라이언트의 입력을 기다리고 있음
     // GET http://localhost:9020/index.html을 clientfd에 입력해주면 연결된 connfd에 전송되고 connfd와 연결된 rio에 전송되고 buf에 담김
     Rio_readlineb(&rio, buf, MAXLINE);                                  /* buf에 fd에서 읽을 값이 담김 */
     sscanf(buf, "%s %s %s", method, uri, version);                      /* sscanf는 첫 번째 매개 변수가 우리가 입력한 문자열, 두 번째 매개 변수 포맷, 나머지 매개 변수에 포맷에 맞게 데이터를 읽어서 인수들에 저장 */
+    strcpy(url, uri);                                                   /* url에 uri 복사 */
 
-    // 캐시에 데이터 써주는 건 여기에서 해주면 될 듯
-
-    /* GET이나 HEAD가 아닐 때 error 메시지 출력 */
-    // 같으면 0이므로 둘 다 0이면 false가 되어 안에 내용이 실행되지 않음
-    // 하나만 같고 다른 건 달라서 0, 1 이면 && 연산에 의해 둘 다 1은 아니므로 False가 됨 -> 즉 하나만 GET이거나 HEAD여도 안에 내용 실행 안 됨
-    // 둘 다 달라서 0이 아니면 True가 됨 -> 즉 둘 다 method가 GET, HEAD가 아닐 때 안에 내용이 실행되는 것
-    if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD")) {
+    /* GET가 아닐 때 error 메시지 출력 */
+    // 같으면 0이므로 같으면 false가 되어 안에 내용이 실행되지 않음 -> 달라야 실행됨
+    if (strcasecmp(method, "GET")) {
         printf("501 Not implemented Tiny does not implement this method");
         return;
+    }
+
+    /* cache에서 url을 가진 것 찾았을 때 cache hit */
+    if (reader(connfd, url)) {
+        return; // doit() 종료
     }
 
     // 캐시에서 데이터 찾았으면 rio_written으로 캐시에 있는 값 보내줌 -> return  (서버에 연결할 필요 없으니까 close할 필요 없음)
@@ -129,7 +146,7 @@ void doit(int connfd) {
     /* Tiny 서버와 프록시 서버가 connect하는 함수 */
     // endserver_http_header에 프록시에서 파싱해준 요청 헤더가 담겨있음
     // 서버와 프록시가 연결되어 새로운 연결 식별자가 생겨남
-    end_serverfd = connect_endServer(hostname, port, endserver_http_header);
+    end_serverfd = connect_endServer(hostname, port);
 
     /* 서버와 연결 실패 */
     if(end_serverfd < 0) {
@@ -154,10 +171,26 @@ void doit(int connfd) {
 
     /* server_rio의 내부 버퍼(end_serverfd와 연결되어 있음)에 담긴 내용을 buf에 담고 클라이언트(리얼 사용자)와 proxy에 연결되어 있는 식별자 connfd에 buf에 담긴 내용을 씀 */
     size_t n;
-    while((n = rio_readlineb(&server_rio, buf, MAXLINE)) != 0) {
+    int total_size = 0;
+
+    while((n = rio_readlineb(&server_rio, buf, MAXLINE)) != 0) { // 리턴 값인 n은 buf의 크기 (읽은 크기)
         printf("proxy received %ld bytes,then send\n",n);
-        Rio_writen(connfd, buf, n);
+        Rio_writen(connfd, buf, n); // 서버에서 보내준 값이 end_serverfd와 연결된 server_rio에 담기고 그것을 buf가 한 줄씩 읽는데, 그걸 connfd에 써서 클라이언트가 보게 함
+
+        /* cache content의 최대 크기를 넘지 않으면 content_buf에 담음 */
+        /* content_buf는 서버에서 받아온 내용, 즉 cache에 쓸 내용을 담은 버퍼 */
+        if (total_size + n < MAX_OBJECT_SIZE) { // 현재 캐쉬에 들어있는 객체의 total_size와 서버에서 보내준 것을 한 줄 더했을 때 MAX보다 적으면
+            strcpy(content_buf + total_size, buf); // 서버가 보내준 한 줄을 content_buf + total_size에 복사
+        }
+        total_size += n;
     }
+
+    /* cache content의 최대 크기를 넘지 않았다면 cache에 쓰기 */
+    // 현재 들어오는 buf의 크기를 total size와 더했을 때 전체 캐시의 크기보다 크면 못 넣고 사라짐
+    if(total_size < MAX_OBJECT_SIZE) {
+        writer(url, content_buf);
+    }
+
     /* end_serverfd 닫기 */
     Close(end_serverfd);
 }
@@ -262,7 +295,7 @@ void build_http_header(char *http_header, char *hostname, char *path, int port, 
 
 /* 서버와 proxy와 연결 */
 // 연결하고 싶은 서버의 ip주소, 포트 번호, 프록시에서 파싱해준 요청 헤더
-int connect_endServer(char *hostname, int port, char *http_header) {
+int connect_endServer(char *hostname, int port) {
     char portStr[100];
     // 포트 번호를 "%d"에 담고 그걸 postStr에 담음
     sprintf(portStr, "%d", port);
@@ -273,4 +306,87 @@ int connect_endServer(char *hostname, int port, char *http_header) {
     // (중요) 프록시와 서버 사이에 Accept이 되고 서버는 Rio_readlineb에서 write가 일어날 때까지 기다리고 있음
     // 연결이 되었으니까 연결 식별자를 리턴하는 것 (듣기 식별자는 서버에 있는 것)
     return Open_clientfd(hostname, portStr);
+}
+
+/* cache 초기화 */
+void init_cache() {
+    Sem_init(&mutex, 0, 1);                                                 /* mutex를 1로 초기화 */
+    Sem_init(&w, 0, 1);                                                     /* w를 1로 초기화 */
+    readcnt = 0;                                                            /* 현재 크리티컬 섹션에 있는 reader의 수를 세는 변수 */
+    cache = (Cache_info *)Malloc(sizeof(Cache_info) * 10);                  /* 캐시의 최대 크기는 1MB이고 캐시의 객체는 최대 크기가 100KB이라서 10개의 공간을 잡음 */
+    for (int i = 0; i < 10; i++) {
+        cache[i].url = (char *)Malloc(sizeof(char) * 256);                  /* url 공간을 256바이트 할당 */
+        cache[i].flag = (int *)Malloc(sizeof(int));                         /* flag 공간을 4바이트 할당 */
+        cache[i].cnt = (int *)Malloc(sizeof(int));                          /* cnt 공간을 4바이트 할당 */
+        cache[i].content = (char *)Malloc(sizeof(char) * MAX_OBJECT_SIZE);  /* content의 공간을 100KB 할당 */
+        *(cache[i].flag) = 0;                                               /* flag 0으로 설정, 즉, 비어있다는 뜻 */
+        *(cache[i].cnt) = 1;                                                /* cnt 0으로 설정, 최근에 찾은 것일 수록 0이랑 가까움 */
+    }
+}
+
+/* cache에서 요청한 url 있는지 찾기 */
+/* 세마포어를 이용해서 reader가 먼저 되고 여러 thread가 읽고 있으면 writer는 할 수가 없게 한다. */
+int reader(int connfd, char *url) {
+    int return_flag = 0;    /* 캐시에서 찾았으면 1, 못 찾으면 0 */
+    P(&mutex); // readcnt를 한번에 하나의 thread만 갱신하도록              
+    readcnt++; // 현재 크리티컬 섹션에 있는 reader의 수를 세는 변수 
+    if(readcnt == 1) {
+        P(&w); // 여러 thread가 read하는 동안 write 방지
+    }
+    V(&mutex);
+
+    /* cache를 다 돌면서 cache에 써있고 cache의 url과 현재 요청한 url이 같으면 client fd에 cache의 내용 쓰고 해당 cache의 cnt를 0으로 초기화 후 break */
+    // 가장 최근에 방문한 cache가 0에 가깝고 오래 전에 방문한 cache는 수가 커짐 -> cache의 용량 초과할 때마다 cnt가 큰 수부터 삭제해줌
+    for(int i = 0; i < 10; i++) {
+        if(*(cache[i].flag) == 1 && !strcmp(cache[i].url, url)) {  // 캐시 객체에 값이 들어있을 때만 찾아야 하므로 1일 때 && 같은 거 찾았을 때 0이니까 not을 붙여서 찾아준 것
+            Rio_writen(connfd, cache[i].content, MAX_OBJECT_SIZE); // connfd에 cache에 들어있는 내용을 써줌
+            return_flag = 1;                                       // cache에서 찾았을 때 더이상 doit()이 실행되지 않도록 하기 위해 return값을 주려고 return_flag에 1을 줌
+            *(cache[i].cnt) = 1;                                   // 최근에 방문한 곳이므로 1로 초기화 해줌
+            break;
+        }
+    }    
+    
+    /* 모든 cache객체의 cnt를 하나씩 올려줌, 즉, 방문 안 한 객체들의 cnt를 하나씩 올려줌*/
+    /* LRU 알고리즘 (least recently used : 가장 오랫동안 참조되지 않은 객체를 교체)
+     * 프로세스가 주기억장치에 접근할 때마다 참조된 객체에 대한 시간을 기록해야함 -> 큰 오버헤드가 발생
+     */
+    for(int i = 0; i < 10; i++) {
+        (*(cache[i].cnt))++;
+    }
+
+    P(&mutex);
+    readcnt--;                                                      // 현재 크리티컬 섹션에 있는 reader의 수가 0일 때 -> 쓰기 가능해지도록 풀어줌
+    if(readcnt == 0) {
+        V(&w);
+    }
+    V(&mutex);
+    return return_flag;
+}
+
+/* cache에서 요청한 url의 정보 쓰기 */
+/* 세마포어를 이용해서 writer는 한번에 하나의 thread만 접근 가능 */
+void writer(char *url, char *buf) {
+    P(&w);
+
+    int idx = 0;                        /* 작성할 곳을 가리키는 index */
+    int max_cnt = 0;                    /* 가장 오래 방문 안한 일수 */
+
+    /* 10개의 cache를 돌고 만약 비어있는 곳이 있으면 비어있는 곳에 index를 찾고, 없으면 가장 오래 방문 안한 곳의 index 찾기 */
+    for(int i = 0; i < 10; i++) {
+        if(*(cache[i].flag) == 0) {     /* 캐시 안에 값이 들어있지 않은 공간이 있으면 */
+            idx = i;
+            break;
+        }
+        if(*(cache[i].cnt) > max_cnt) { /* 캐시가 다 차 있을 때는 가장 오래 방문 안한 공간을 찾아야 함 (가장 큰 cnt 값을 찾음) */
+            idx = i;
+            max_cnt = *(cache[i].cnt);
+        }
+    }
+    /* 해당 index에 cache 작성 */
+    *(cache[idx].flag) = 1;             /* 써줘서 공간이 찼으므로 1 */
+    strcpy(cache[idx].url, url);        /* cache의 url에 url 복사 */
+    strcpy(cache[idx].content, buf);    /* cache의 content에 buf에 있는 내용 복사 */
+    *(cache[idx].cnt) = 1;              /* 가장 최근에 방문했으므로 cnt 1로 설정 */
+
+    V(&w);
 }
